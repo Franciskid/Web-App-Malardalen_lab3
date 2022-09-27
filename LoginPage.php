f1946bed619a94da93530f34be844554c849d889
Thomas Culino <thomas.culino@edu.devinci.fr>
2022-09-27 21:27:21 +0200

    link php database

--- LoginPage.php
+++ LoginPage.php
@@ -29,7 +29,7 @@ if (session_status() === PHP_SESSION_NONE) {
     </div>
   </div>
   <div class="content">
-    <form name="loginForm" action="javascript:changeURL()" onsubmit="return validationLoginForm()">
+    <form name="loginForm" action="./LoginPage.php">
       <label for="uname"><b>Username</b></label>
       <input type="text" placeholder="Enter Username" name="username" required>
 
@@ -38,39 +38,31 @@ if (session_status() === PHP_SESSION_NONE) {
       <button type="submit" class="gobackButton">Login</button>
     </form>
     <?php
+
       $db = new mysqli("localhost", "root", "root", "assignment3");
       if ($db->connect_error) {
         die("Could not connect: " . mysqli_connect_error());
       }
 
-      $users = $db->query("SELECT * FROM users");
-      while($row = $users->fetch_assoc()) {
-        echo $row["username"];
-        echo '<br>';
+      if (isset($_GET['username'])) {
+        $username = $_GET['username'];
+        $user = $db->query("SELECT * FROM users WHERE username = '$username'");
+        if ($user->num_rows > 0) {
+          $password = $user->fetch_assoc()['password'];
+          if ($password === $_GET['password']) {
+            echo "<script>window.location.href = './AdminPage.php';</script>";
+          }
+          else {
+            echo "<script>alert('The username or password is incorrect !');</script>";
+          }
+        }
+      }
+      else if (isset($_GET['password'])) {
+        echo "<script>alert('The username or password is incorrect !');</script>";
       }
     
-    $db->close();
+      $db->close();
     ?>
-    <script>
-      function validationLoginForm() {
-        var x = document.forms["loginForm"]["username"].value;
-        var y = document.forms["loginForm"]["password"].value;
-        if (x == "" || y == "") {
-          alert("Username or password is incorrect !");
-          return false;
-        }
-      }
-
-      function changeURL() {
-        var x = document.forms["loginForm"]["username"].value;
-        var y = document.forms["loginForm"]["password"].value;
-        console.log(x.length);
-        console.log(y.length);
-        if (x.length !== 0 && y.length != 0) {
-          window.location.href = "./AdminPage.php";
-        }
-      }
-    </script>
     <label id="rememberme">
       <input type="checkbox" name="remember" <?php if (isset($_SESSION["rememberme"]) and $_SESSION["rememberme"] != "") { ?> checked <?php } ?>> Remember me
     </label>
--- Ressources/lab3_db.sql
+++ Ressources/lab3_db.sql
@@ -3,10 +3,9 @@ USE assignment3;
 
 CREATE TABLE users
 (
-id INT NOT NULL AUTO_INCREMENT,
 username VARCHAR(20) NOT NULL,
 password VARCHAR(20) NOT NULL,
-PRIMARY KEY (id)
+PRIMARY KEY (username)
 );
 
 INSERT INTO users (username, password) VALUES ('tculino', 'thomas');
